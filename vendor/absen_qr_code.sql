24055b2e001a5ffb726bf05073e6fb23737020d0
arlanbutarbutar <arlan270899@gmail.com>
2022-12-20 07:42:34 +0800

    udpate

--- vendor/absen_qr_code.sql
+++ vendor/absen_qr_code.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Nov 2022 pada 14.10
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 7.4.27
+-- Waktu pembuatan: 20 Des 2022 pada 00.42
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,14 @@ CREATE TABLE `absen` (
   `status` varchar(20) NOT NULL DEFAULT 'Hadir'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `absen`
+--
+
+INSERT INTO `absen` (`id_absen`, `id_jadwal`, `nim_mhs`, `jam_masuk`, `tgl_masuk`, `status`) VALUES
+(11, 7, 32443, '15:17:24', '2022-12-07', 'Hadir'),
+(12, 8, 32443, '07:16:01', '2022-12-20', 'Hadir');
+
 -- --------------------------------------------------------
 
 --
@@ -55,7 +63,9 @@ CREATE TABLE `dosen` (
 --
 
 INSERT INTO `dosen` (`nip_dosen`, `nama_dosen`, `jenis_kelamin`, `gelar`, `jabatan`) VALUES
-('12345678', 'Berty', 'P', 'S.Kom', 'admin');
+('12345678', 'Berty', 'P', 'S.Kom', 'admin'),
+('23118036', 'Arlan Butar Butar', 'L', 'S. Kom', 'Kepro'),
+('41321341', 'Arlan', 'L', 'S. Kom', 'Kepro');
 
 -- --------------------------------------------------------
 
@@ -67,22 +77,44 @@ CREATE TABLE `jadwal` (
   `id_jadwal` int(11) NOT NULL,
   `id_mk` int(11) NOT NULL,
   `hari` varchar(20) NOT NULL,
-  `ruang` varchar(20) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
   `mulai` time NOT NULL,
   `selesai` time NOT NULL,
   `qr_code` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `jadwal`
+--
+
+INSERT INTO `jadwal` (`id_jadwal`, `id_mk`, `hari`, `id_kelas`, `mulai`, `selesai`, `qr_code`) VALUES
+(5, 10, 'Rabu', 7, '15:12:00', '17:12:00', '7988.jpg'),
+(6, 10, 'Kamis', 8, '12:12:00', '14:12:00', '3775.jpg'),
+(7, 11, 'Rabu', 8, '15:16:00', '16:16:00', '4973.jpg'),
+(8, 12, 'Selasa', 10, '07:14:00', '08:14:00', '2538.jpg'),
+(9, 12, 'Senin', 9, '09:30:00', '11:45:00', '1867.jpg');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jurusan`
+-- Struktur dari tabel `kelas`
 --
 
-CREATE TABLE `jurusan` (
-  `id_jurusan` int(11) NOT NULL,
-  `nama_jurusan` varchar(100) NOT NULL
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `id_prodi` int(11) NOT NULL,
+  `nama_kelas` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `id_prodi`, `nama_kelas`) VALUES
+(7, 7, 'A1'),
+(8, 7, 'A2'),
+(9, 8, 'B1'),
+(10, 8, 'A1');
 
 -- --------------------------------------------------------
 
@@ -92,14 +124,22 @@ CREATE TABLE `jurusan` (
 
 CREATE TABLE `mahasiswa` (
   `nim_mhs` int(11) NOT NULL,
-  `id_prodi` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
   `nama_mhs` varchar(100) NOT NULL,
   `tempat_lahir` varchar(50) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `agama` varchar(35) NOT NULL,
   `alamat` varchar(225) NOT NULL,
-  `no_hp` char(12) NOT NULL
+  `no_hp` char(12) NOT NULL,
+  `semester` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim_mhs`, `id_kelas`, `nama_mhs`, `tempat_lahir`, `tanggal_lahir`, `agama`, `alamat`, `no_hp`, `semester`) VALUES
+(32443, 8, 'tes', 'kpg', '2022-12-07', 'Islam', '72GV+PFP, Namodale, Kec. Lobalain, Kabupaten Rote Ndao, Nusa Tenggara Tim.', '2342312', 0);
 
 -- --------------------------------------------------------
 
@@ -114,6 +154,15 @@ CREATE TABLE `mata_kuliah` (
   `sks` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `mata_kuliah`
+--
+
+INSERT INTO `mata_kuliah` (`id_mk`, `nip_dosen`, `nama_matakuliah`, `sks`) VALUES
+(10, '41321341', 'Kalkulus', 3),
+(11, '41321341', 'Web 2', 3),
+(12, '23118036', 'QA', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -122,9 +171,18 @@ CREATE TABLE `mata_kuliah` (
 
 CREATE TABLE `prodi` (
   `id_prodi` int(11) NOT NULL,
-  `id_jurusan` int(11) NOT NULL,
   `nama_prodi` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `prodi`
+--
+
+INSERT INTO `prodi` (`id_prodi`, `nama_prodi`) VALUES
+(7, 'Teknik Informatika'),
+(8, 'Cyber Security'),
+(10, 'Teknik Sipil'),
+(11, 'Teknik Elektro');
 
 --
 -- Indexes for dumped tables
@@ -149,20 +207,22 @@ ALTER TABLE `dosen`
 --
 ALTER TABLE `jadwal`
   ADD PRIMARY KEY (`id_jadwal`),
-  ADD KEY `id_mk` (`id_mk`);
+  ADD KEY `id_mk` (`id_mk`),
+  ADD KEY `jadwal_ibfk_2` (`id_kelas`);
 
 --
--- Indeks untuk tabel `jurusan`
+-- Indeks untuk tabel `kelas`
 --
-ALTER TABLE `jurusan`
-  ADD PRIMARY KEY (`id_jurusan`);
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD KEY `id_prodi` (`id_prodi`);
 
 --
 -- Indeks untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`nim_mhs`),
-  ADD KEY `id_jurusan` (`id_prodi`);
+  ADD KEY `mahasiswa_ibfk_1` (`id_kelas`);
 
 --
 -- Indeks untuk tabel `mata_kuliah`
@@ -174,8 +234,7 @@ ALTER TABLE `mata_kuliah`
 -- Indeks untuk tabel `prodi`
 --
 ALTER TABLE `prodi`
-  ADD PRIMARY KEY (`id_prodi`),
-  ADD KEY `id_fakultas` (`id_jurusan`);
+  ADD PRIMARY KEY (`id_prodi`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -185,19 +244,19 @@ ALTER TABLE `prodi`
 -- AUTO_INCREMENT untuk tabel `absen`
 --
 ALTER TABLE `absen`
-  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `jurusan`
+-- AUTO_INCREMENT untuk tabel `kelas`
 --
-ALTER TABLE `jurusan`
-  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `mahasiswa`
@@ -209,13 +268,13 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT untuk tabel `mata_kuliah`
 --
 ALTER TABLE `mata_kuliah`
-  MODIFY `id_mk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_mk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `prodi`
 --
 ALTER TABLE `prodi`
-  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -232,19 +291,20 @@ ALTER TABLE `absen`
 -- Ketidakleluasaan untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_mk`) REFERENCES `mata_kuliah` (`id_mk`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_mk`) REFERENCES `mata_kuliah` (`id_mk`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `jadwal_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `kelas`
+--
+ALTER TABLE `kelas`
+  ADD CONSTRAINT `kelas_ibfk_1` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Ketidakleluasaan untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Ketidakleluasaan untuk tabel `prodi`
---
-ALTER TABLE `prodi`
-  ADD CONSTRAINT `prodi_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id_jurusan`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
